6d775b7ad0c98a9965311dfd9ada5bd59e04abe8
Lakshay-Motla87 <lakshaymotla87@gmail.com>
2025-09-07 22:23:08 +0530

    Initial Commit

---
+++ DS Training/Day3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int>v;
+    v.pushback(1);
+    cout<<"size of the vector 1:"<<v.size()<< endl;
+    cout<<"capacity of water:"<<v.capacity()<< endl;
+
+    for(int i=0;i<v.size();i++)
+    //cout<<v(i)<<("  ");
+    cout<<v.at(i)<<("  ");
+    cout<<endl;
+
+}
